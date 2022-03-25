bbe223bed4da1c8a7f0cd6c0422426e4876e7217
Kim Taavo <kim.taavo@digdir.no>
2022-03-25 11:31:12 +0100

    wip

--- vars/pipelineWithMavenAndDocker.groovy
+++ vars/pipelineWithMavenAndDocker.groovy
@@ -135,11 +135,13 @@ def call(body) {
                         post {
                             failure {
                                 script {
+                                    echo 'Fisheye is disabled, create review as Github Pull Request'
                                   //  components.prepareVerification.failureScript(params)
                                 }
                             }
                             aborted {
                                 script {
+                                    echo 'Fisheye is disabled, create review as Github Pull Request'
                                     //components.prepareVerification.abortedScript(params)
                                 }
                             }
